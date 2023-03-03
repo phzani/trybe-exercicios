ac1a0de9dbdc8a73f845ccf0472955fdf0e92ea0
Pedro Zani <ph.zani@icloud.com>
2023-03-03 19:48:43 -0300

    exercicios-dia04

---
+++ 01-fundamentos/secao03-javascript/dia04-JSON/para-fixar.js
@@ -0,0 +1,42 @@
+// Com base neste objeto, faça os exercícios a seguir.
+
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+// Imprima no console uma mensagem de boas-vindas para a personagem acima, na qual tenha o nome dela. Para isso, utilize a sintaxe meuObjeto.chave. Então, o valor esperado no console é:
+console.log("Bem Vinda, " + info["personagem"]);
+
+//Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. Para isso, use a sintaxe meuObjeto['chave'] = valor. Então, o valor esperado no console é:
+
+info["recorrente"] = "Sim";
+console.log(info);
+
+// Faça um for/in que mostre todas as chaves do objeto. O valor esperado no console é:
+
+for (let key in info) {
+  console.log(key);
+}
+
+//Faça um novo for/in, mas agora mostre todos os valores das propriedades do objeto. O valor esperado no console é:
+for (let value in info) {
+  console.log(info[value]);
+}
+
+//Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: “Tio Patinhas”, “Christmas on Bear Mountain, Dell’s Four Color Comics #178”, “O último MacPatinhas”, “Sim”. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. O valor esperado no console é:
+
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell’s Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim",
+};
+
+for (let keys in info) {
+  if (keys === "recorrente" && info[keys] === "Sim" && info2[keys] === "Sim") {
+    console.log("Ambos recorrentes");
+  } else {
+    console.log(info[keys] + " e " + info2[keys]);
+  }
+}
