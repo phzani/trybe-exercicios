6c89f91bea673dfa37fe6a952892143d223f64c8
Pedro Zani <ph.zani@icloud.com>
2023-03-06 12:01:57 -0300

    exercicio ES6

--- 01-fundamentos/secao03-javascript/dia04-JSON/para-fixar.js
+++ 01-fundamentos/secao03-javascript/dia04-JSON/para-fixar.js
@@ -40,3 +40,32 @@ for (let keys in info) {
     console.log(info[keys] + " e " + info2[keys]);
   }
 }
+
+//Com base neste objeto, faça os exercícios a seguir.
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+//Acesse as chaves nome, sobrenome e titulo – esta encontra-se dentro da chave livrosFavoritos – e faça um console.log no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”.
+console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor.livrosFavoritos[0].titulo}`);
+
+// Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a essa chave um objeto que contenha as seguintes informações:
+
+leitor.livrosFavoritos.push({
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editora: 'Rocco',
+})
+
+const quantidadeLivros = leitor.livrosFavoritos.length;
+console.log(`${leitor.nome} tem ${quantidadeLivros} livros favoritos`);
